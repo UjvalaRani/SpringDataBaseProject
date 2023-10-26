bdd884966cf3d8d1e044dc07a6a0891e3b9f13df
UjvalaRani <128087597+UjvalaRani@users.noreply.github.com>
2023-10-26 23:54:37 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringDataBaseProject/SpringDataBaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataBaseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataBaseProjectApplication.class, args);
+	}
+
+}
